39a3d2e642243863e28b8b6e6519c2ff104f43d8
Anthony Lukach <anthonylukach@gmail.com>
2020-02-10 11:10:20 -0700

    Updates

---
+++ precache-manifest.aa45053ed65792daf903831537fec707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f227c501160997d454f377ab104359ba",
+    "url": "/ds-news/index.html"
+  },
+  {
+    "revision": "4873a405c052d847f0b8",
+    "url": "/ds-news/static/css/main.70e20ff1.chunk.css"
+  },
+  {
+    "revision": "eab6dfdf4bb917c12af4",
+    "url": "/ds-news/static/js/2.b110895b.chunk.js"
+  },
+  {
+    "revision": "a988fa69c5e58f2376c92f812dbc255b",
+    "url": "/ds-news/static/js/2.b110895b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4873a405c052d847f0b8",
+    "url": "/ds-news/static/js/main.26766e57.chunk.js"
+  },
+  {
+    "revision": "6c7951878a2d18b2ee5b",
+    "url": "/ds-news/static/js/runtime-main.6170c3cb.js"
+  }
+]);
