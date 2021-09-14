2bee42d33d72f45829c5238d8c2ed217b964171b
Anthony Lukach <anthonylukach@gmail.com>
2021-09-14 11:59:39 -0600

    Fixup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,25 +12,23 @@ import About from "./views/About";
 import { persistor } from "./store/store";
 import config from "./config";
 
-const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <PersistGate persistor={persistor} loading={<p>Loading...</p>}>
-          <Router basename={config.basePath}>
-            <Nav />
-            <div className="container-fluid">
-              <Switch>
-                <Route path={config.paths.login} component={Login} />
-                <Route path={config.paths.about} component={About} />
-                <Route path={config.paths.feed} component={Feed} />
-              </Switch>
-            </div>
-          </Router>
-        </PersistGate>
-      </ConnectedRouter>
-    </Provider>
-  );
-};
+const App: React.FC = () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <PersistGate persistor={persistor} loading={<p>Loading...</p>}>
+        <Router basename={config.basePath}>
+          <Nav />
+          <div className="container-fluid">
+            <Switch>
+              <Route path={config.paths.login} component={Login} />
+              <Route path={config.paths.about} component={About} />
+              <Route path={config.paths.feed} component={Feed} />
+            </Switch>
+          </div>
+        </Router>
+      </PersistGate>
+    </ConnectedRouter>
+  </Provider>
+);
 
 export default App;
