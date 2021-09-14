f3458f29da6fe5af32887d0c17b23b344b4ce416
Anthony Lukach <anthonylukach@gmail.com>
2021-09-14 12:48:39 -0600

    Add spinner

--- frontend/src/views/Feed/NavFilter.tsx
+++ frontend/src/views/Feed/NavFilter.tsx
@@ -7,62 +7,76 @@ import {
 } from "reactstrap";
 import { QueryState } from "../../store/query/query.reducer";
 
-export default ({ sort, setSort, searchTerm, setSearchTerm, isFetching, triggerSearch }: Props) => {
-  const sortOptions = {
-    created: "recently created",
-    updated: "recently updated",
-    interactions: "interactions",
-    "reactions-heart": "loved",
-    "reactions-tada": "celebrated",
-    reactions: "reactions",
-    "reactions-+1": "liked",
-    "reactions--1": "disliked",
-  };
-  return (
-    <nav className="navbar px-0">
-      <ul className="d-flex" style={{ flexGrow: 1 }}>
-        <li className="w-100">
-          <div className="input-group input-group-sm ">
-            <input
-              placeholder="Search"
-              value={searchTerm}
-              className={`form-control text-monospace w-100 ${isFetching ? 'loading' : ''}`}
-              onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                setSearchTerm(e.currentTarget.value)
-              }
-            />
-            <div className="input-group-append">
-              <button 
-                className="btn btn-outline-secondary" 
-                type="button" 
-                disabled={isFetching}
-                onClick={() => triggerSearch()}
-              >
-                &#x21bb;
-              </button>
-            </div>
-          </div>
-        </li>
-      </ul>
-      <ul className="ml-auto">
-        <li className="ml-2">
-          <UncontrolledDropdown className="d-inline">
-            <DropdownToggle tag="a" className="link-button d-inline-block">
-              most {sortOptions[sort as keyof typeof sortOptions]} ▼
-            </DropdownToggle>{" "}
-            <DropdownMenu>
-              {Object.entries(sortOptions).map(([key, value]) => (
-                <DropdownItem key={key} onClick={() => setSort(key)}>
-                  {value}
-                </DropdownItem>
-              ))}
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </li>
-      </ul>
-    </nav>
-  );
+const sortOptions = {
+  created: "recently created",
+  updated: "recently updated",
+  interactions: "interactions",
+  "reactions-heart": "loved",
+  "reactions-tada": "celebrated",
+  reactions: "reactions",
+  "reactions-+1": "liked",
+  "reactions--1": "disliked",
 };
+
+export default ({
+  sort,
+  setSort,
+  searchTerm,
+  setSearchTerm,
+  isFetching,
+  triggerSearch,
+}: Props) => (
+  <nav className="navbar px-0">
+    <ul className="d-flex" style={{ flexGrow: 1 }}>
+      <li className="w-100">
+        <div className="input-group input-group-sm ">
+          <input
+            placeholder="Search"
+            value={searchTerm}
+            className={`form-control text-monospace w-100 ${
+              isFetching ? "loading" : ""
+            }`}
+            onChange={(e: React.FormEvent<HTMLInputElement>) =>
+              setSearchTerm(e.currentTarget.value)
+            }
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              disabled={isFetching}
+              onClick={() => triggerSearch()}
+            >
+              {isFetching ? (
+                <div className="spinner-border spinner-border-sm" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              ) : 
+                <span className="pl-1">↻</span>
+              }
+            </button>
+          </div>
+        </div>
+      </li>
+    </ul>
+    <ul className="ml-auto">
+      <li className="ml-2">
+        <UncontrolledDropdown className="d-inline">
+          <DropdownToggle tag="a" className="link-button d-inline-block">
+            most {sortOptions[sort as keyof typeof sortOptions]} ▼
+          </DropdownToggle>{" "}
+          <DropdownMenu>
+            {Object.entries(sortOptions).map(([key, value]) => (
+              <DropdownItem key={key} onClick={() => setSort(key)}>
+                {value}
+              </DropdownItem>
+            ))}
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </li>
+    </ul>
+  </nav>
+);
 
 interface Props {
   sort: QueryState["query"]["sort"];
