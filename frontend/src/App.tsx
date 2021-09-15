88f4923af1fe86a95802531b29e34d3dae889b59
Anthony Lukach <anthonylukach@gmail.com>
2021-09-14 22:39:31 -0600

    Add auto-dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Provider } from "react-redux";
 import { store, history } from "./store/store";
 import { ConnectedRouter } from "connected-react-router";
@@ -12,23 +12,42 @@ import About from "./views/About";
 import { persistor } from "./store/store";
 import config from "./config";
 
-const App: React.FC = () => (
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <PersistGate persistor={persistor} loading={<p>Loading...</p>}>
-        <Router basename={config.basePath}>
-          <Nav />
-          <div className="container-fluid">
-            <Switch>
-              <Route path={config.paths.login} component={Login} />
-              <Route path={config.paths.about} component={About} />
-              <Route path={config.paths.feed} component={Feed} />
-            </Switch>
-          </div>
-        </Router>
-      </PersistGate>
-    </ConnectedRouter>
-  </Provider>
-);
+const App: React.FC = () => {
+  const [isDark, setDark] = useState(false);
+  useEffect(() => {
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+    setDark(isDark.matches);
+    isDark.addEventListener('change', (event: MediaQueryListEvent) => {
+      setDark(event.matches);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add('dark')
+    } else {
+      document.body.classList.remove('dark')
+    }
+  }, [isDark])
+  
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <PersistGate persistor={persistor} loading={<p>Loading...</p>}>
+          <Router basename={config.basePath}>
+            <Nav />
+            <div className="container-fluid">
+              <Switch>
+                <Route path={config.paths.login} component={Login} />
+                <Route path={config.paths.about} component={About} />
+                <Route path={config.paths.feed} component={Feed} />
+              </Switch>
+            </div>
+          </Router>
+        </PersistGate>
+      </ConnectedRouter>
+    </Provider>
+  );
+};
 
 export default App;
