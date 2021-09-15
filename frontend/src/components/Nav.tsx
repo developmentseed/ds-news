39e0d7e59b7a45dcab84d5254a263acb9bd19e26
Anthony Lukach <anthonylukach@gmail.com>
2021-09-15 09:16:34 -0600

    Add button for dark/light mode

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { bindActionCreators } from "redux";
 import qs from "query-string";
 import { connect } from "react-redux";
@@ -8,6 +8,23 @@ import { NavLink, useLocation } from "react-router-dom";
 import config from "../config";
 
 export const Nav: React.SFC<Props> = ({ isLoggedIn, dispatchLogout }) => {
+  const [isDark, setDark] = useState(false);
+  useEffect(() => {
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)");
+    setDark(isDark.matches);
+    isDark.addEventListener("change", (event: MediaQueryListEvent) => {
+      setDark(event.matches);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [isDark]);
+
   const { search } = useLocation();
   const loginUrl = qs.stringifyUrl({
     url: "https://github.com/login/oauth/authorize",
@@ -49,6 +66,13 @@ export const Nav: React.SFC<Props> = ({ isLoggedIn, dispatchLogout }) => {
         </li> */}
       </ul>
       <ul>
+        <li>
+          {
+            <span className="btn btn-sm" onClick={() => setDark(!isDark)}>
+              {isDark ? "🌙" : "☀️"}
+            </span>
+          }
+        </li>
         <li>
           {isLoggedIn ? (
             <button onClick={dispatchLogout} className="link-button">
